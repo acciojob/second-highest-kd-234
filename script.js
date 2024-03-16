91f97bc6a2436175303a98e5caa52f40d064f9ac
Kuldeep kumar <117909055+kd-234@users.noreply.github.com>
2024-03-16 14:39:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+			//Write your code here
+			let big arr[0]
+let sbig = -Infinity
+
+for (let t of arr)
+if(t>big){
+sbig =big
+big = t}
+
+else if(t>sbig &&  big!= t){
+  sbig = t
+}
+return (sbig)
+		}
