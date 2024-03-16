90f00d4847c9ab210aaa33af873b538cf2180403
Kuldeep kumar <117909055+kd-234@users.noreply.github.com>
2024-03-16 14:53:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 //your JS code here. If required.
 function secondHighest(arr) {
 			//Write your code here
-			let big arr[]
-let sbig = -Infinity
-
-for (let t of arr)
-if(t>big){
-sbig =big
-big = t}
-
-else if(t>sbig &&  big!= t){
-  sbig = t
+			if(arr.length <= 1) return -Infinity;
+	let largest = -Infinity;
+let secondLargest = null;
+	for (let i = 0; i < arr.length; i++) {
+    let num = parseInt(arr[i]);
+		if (num > largest) {
+    secondLargest = largest;
+    largest = num;
+} else if (num < largest && (secondLargest === null || num > secondLargest)) {
+    secondLargest = num;
 }
-
-		}
-		return(sbig)
-
+		return secondLargest;
